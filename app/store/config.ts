05ad5720ba120c00cd4ac009bc1cae9b430f3ed9
Shadowlee <S0urceC0der@users.noreply.github.com>
2024-08-09 17:05:56 +0800

    Update config.ts

--- app/store/config.ts
+++ app/store/config.ts
@@ -38,7 +38,7 @@ export const DEFAULT_CONFIG = {
   theme: Theme.Auto as Theme,
   tightBorder: !!config?.isApp,
   sendPreviewBubble: true,
-  enableAutoGenerateTitle: false,
+  enableAutoGenerateTitle: true,
   sidebarWidth: DEFAULT_SIDEBAR_WIDTH,
 
   disablePromptHint: false,
