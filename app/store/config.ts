337d390ba7a35da02ebff0e53f7863ea9e9ef581
Shadowlee <S0urceC0der@users.noreply.github.com>
2024-08-09 10:34:09 +0800

    Update config.ts
    decrease history count to 0, for duckduck-ai

--- app/store/config.ts
+++ app/store/config.ts
@@ -58,7 +58,7 @@ export const DEFAULT_CONFIG = {
     presence_penalty: 0,
     frequency_penalty: 0,
     sendMemory: true,
-    historyMessageCount: 4,
+    historyMessageCount: 0,
     compressMessageLengthThreshold: 1000,
     enableInjectSystemPrompts: true,
     template: config?.template ?? DEFAULT_INPUT_TEMPLATE,
@@ -144,7 +144,7 @@ export const useAppConfig = createPersistStore(
 
       if (version < 3.4) {
         state.modelConfig.sendMemory = true;
-        state.modelConfig.historyMessageCount = 4;
+        state.modelConfig.historyMessageCount = 0;
         state.modelConfig.compressMessageLengthThreshold = 1000;
         state.modelConfig.frequency_penalty = 0;
         state.modelConfig.top_p = 1;
